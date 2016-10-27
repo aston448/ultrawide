84803655c04f27853504b079e8d4d9996d1996c2
aston <aston.cockayne@blueyonder.co.uk>
2016-10-27 09:45:16 +0100

    Dual environment test

--- imports/ui/components/app/AppHeader.jsx
+++ imports/ui/components/app/AppHeader.jsx
@@ -209,7 +209,7 @@ class AppHeader extends Component {
                 <Grid>
                     <Row className="header-row-top">
                         <Col md={2}>
-                            <div className="ultrawide-logo">ULTRAWIDE</div>
+                            <div className="ultrawide-logo">ULTRAWIDE R</div>
                         </Col>
                         <Col md={3}>
                             {headerUserInfo}
