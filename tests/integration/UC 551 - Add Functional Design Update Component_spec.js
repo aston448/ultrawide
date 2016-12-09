3660e3bdbbee2abf0ed400d7764e97a73ee8b028
aston <aston.cockayne@blueyonder.co.uk>
2016-12-09 12:08:02 +0000

    UC 551 Integration Tests

---
+++ tests/integration/UC 551 - Add Functional Design Update Component_spec.js
@@ -0,0 +1,119 @@
+import {RoleType, ViewMode, ComponentType, DesignVersionStatus, WorkPackageType, WorkPackageStatus} from '../../imports/constants/constants.js'
+import {DefaultItemNames, DefaultComponentNames} from '../../imports/constants/default_names.js';
+
+describe('UC 551 - Add Functional Design Update Component', function(){
+
+    before(function(){
+
+        server.call('testFixtures.clearAllData');
+
+        // Create a Design
+        server.call('testDesigns.addNewDesign', RoleType.DESIGNER);
+        server.call('testDesigns.updateDesignName', RoleType.DESIGNER, DefaultItemNames.NEW_DESIGN_NAME, 'Design1');
+        server.call('testDesigns.selectDesign', 'Design1', 'gloria');
+        // Name and Publish a Design Version
+        server.call('testDesignVersions.selectDesignVersion', DefaultItemNames.NEW_DESIGN_VERSION_NAME, 'gloria');
+        server.call('testDesignVersions.updateDesignVersionName', 'DesignVersion1', RoleType.DESIGNER, 'gloria');
+        server.call('testDesignVersions.publishDesignVersion', 'DesignVersion1', 'gloria', RoleType.DESIGNER);
+        // Add Basic Data to the Design Version
+        server.call('testDesigns.editDesignVersion', 'Design1', 'DesignVersion1', 'gloria', RoleType.DESIGNER);
+        server.call('testFixtures.AddBasicDesignData', 'Design1', 'DesignVersion1');
+        // Complete the Design Version and create the next
+        server.call('testDesignVersions.createNextDesignVersion', 'DesignVersion1', RoleType.DESIGNER, 'gloria');
+        server.call('testDesignVersions.selectDesignVersion', DefaultItemNames.NEXT_DESIGN_VERSION_NAME, 'gloria');
+        server.call('testDesignVersions.updateDesignVersionName', 'DesignVersion2', RoleType.DESIGNER, 'gloria');
+    });
+
+    after(function(){
+
+    });
+
+    beforeEach(function(){
+
+        // Remove any Design Updates before each test
+        server.call('textFixtures.clearDesignUpdates');
+    });
+
+    afterEach(function(){
+
+    });
+
+
+    // Actions
+    it('A Feature can be added to a Design Update Design Section', function(){
+
+        //Setup - add a new Update
+        server.call('testDesignVersions.selectDesignVersion', 'DesignVersion2', 'gloria');
+        server.call('testDesignUpdates.addDesignUpdate', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdates.selectDesignUpdate', DefaultItemNames.NEW_DESIGN_UPDATE_NAME, 'gloria');
+        server.call('testDesignUpdates.updateDesignUpdateName', 'DesignUpdate1', RoleType.DESIGNER, 'gloria');
+
+        // Add new Feature to original Section 1
+        server.call('testDesignUpdates.editDesignUpdate', 'DesignUpdate1', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdateComponents.addFeatureToDesignSection', 'Application1', 'Section1', 'gloria', ViewMode.MODE_EDIT);
+
+        // Verify
+        server.call('verifyDesignUpdateComponents.componentExistsInDesignUpdateWithParentCalled', ComponentType.FEATURE, 'Section1', DefaultComponentNames.NEW_FEATURE_NAME, 'gloria');
+    });
+
+    it('A Scenario can be added to a Design Update Feature', function(){
+
+        //Setup - add a new Update
+        server.call('testDesignVersions.selectDesignVersion', 'DesignVersion2', 'gloria');
+        server.call('testDesignUpdates.addDesignUpdate', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdates.selectDesignUpdate', DefaultItemNames.NEW_DESIGN_UPDATE_NAME, 'gloria');
+        server.call('testDesignUpdates.updateDesignUpdateName', 'DesignUpdate1', RoleType.DESIGNER, 'gloria');
+
+        // Add new Scenario to original Feature 1
+        server.call('testDesignUpdates.editDesignUpdate', 'DesignUpdate1', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdateComponents.addScenarioToFeature', 'Section1', 'Feature1', 'gloria', ViewMode.MODE_EDIT);
+
+        // Verify
+        server.call('verifyDesignUpdateComponents.componentExistsInDesignUpdateWithParentCalled', ComponentType.SCENARIO, 'Feature1', DefaultComponentNames.NEW_SCENARIO_NAME, 'gloria');
+    });
+
+    it('A Scenario can be added to a Design Update Feature Aspect', function(){
+
+        //Setup - add a new Update
+        server.call('testDesignVersions.selectDesignVersion', 'DesignVersion2', 'gloria');
+        server.call('testDesignUpdates.addDesignUpdate', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdates.selectDesignUpdate', DefaultItemNames.NEW_DESIGN_UPDATE_NAME, 'gloria');
+        server.call('testDesignUpdates.updateDesignUpdateName', 'DesignUpdate1', RoleType.DESIGNER, 'gloria');
+
+        // Add new Scenario to original Feature 1 Actions
+        server.call('testDesignUpdates.editDesignUpdate', 'DesignUpdate1', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdateComponents.addScenarioToFeatureAspect', 'Feature1', 'Actions', 'gloria', ViewMode.MODE_EDIT);
+
+        // Verify
+        server.call('verifyDesignUpdateComponents.componentExistsInDesignUpdateWithParentCalled', ComponentType.SCENARIO, 'Actions', DefaultComponentNames.NEW_SCENARIO_NAME, 'gloria');
+    });
+
+
+    // Conditions
+    it('A functional Design Update Component can only be added in edit mode', function(){
+
+        //Setup - add a new Update
+        server.call('testDesignVersions.selectDesignVersion', 'DesignVersion2', 'gloria');
+        server.call('testDesignUpdates.addDesignUpdate', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdates.selectDesignUpdate', DefaultItemNames.NEW_DESIGN_UPDATE_NAME, 'gloria');
+        server.call('testDesignUpdates.updateDesignUpdateName', 'DesignUpdate1', RoleType.DESIGNER, 'gloria');
+
+        // Add Scenario
+        server.call('testDesignUpdates.editDesignUpdate', 'DesignUpdate1', 'gloria', RoleType.DESIGNER);
+        server.call('testDesignUpdateComponents.addScenarioToFeatureAspect', 'Feature1', 'Actions', 'gloria', ViewMode.MODE_EDIT);
+
+        // Verify
+        server.call('verifyDesignUpdateComponents.componentDoesNotExistCalled', ComponentType.SCENARIO, DefaultComponentNames.NEW_SCENARIO_NAME, 'gloria');
+    });
+
+    it('A functional Design Update Component cannot be added to a component removed in this or another Design Update');
+
+
+    // Consequences
+    it('When a functional Design Component is added to a Design Update it is also added to the Design Update Scope');
+
+    it('When a functional Design Component is added to a Draft Design Update it is also added to generic Design Update Work Package scope');
+
+    it('A functional Design Component added to a Design Update appears as a functional addition in the Design Update summary');
+
+});
