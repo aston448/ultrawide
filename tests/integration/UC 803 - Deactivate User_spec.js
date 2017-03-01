63d80ce28147e50f571aca60b96e83d020c401c8
aston <aston.cockayne@blueyonder.co.uk>
2017-03-01 12:44:48 +0000

    UC 803, 804 Integration tests

--- tests/integration/UC 803 - Deactivate User_spec.js
+++ tests/integration/UC 803 - Deactivate User_spec.js
@@ -1,7 +1,14 @@
+import TestFixtures                     from '../../test_framework/test_wrappers/test_fixtures.js';
+import UserManagementActions            from '../../test_framework/test_wrappers/user_management_actions.js';
+import UserManagementVerifications      from '../../test_framework/test_wrappers/user_management_verifications.js';
+
+import { UserManagementValidationErrors } from '../../imports/constants/validation_errors.js'
+import { DefaultUserDetails }           from '../../imports/constants/default_names.js'
+
 describe('UC 803 - Deactivate User', function(){
 
     before(function(){
-
+        TestFixtures.logTestSuite('UC 803 - Deactivate User');
     });
 
     after(function(){
@@ -9,29 +16,63 @@ describe('UC 803 - Deactivate User', function(){
     });
 
     beforeEach(function(){
-
+        TestFixtures.clearAllData();
     });
 
     afterEach(function(){
-
+        // Need to do this or tests will fail due to existing Meteor users
+        TestFixtures.removeAllMeteorUsers();
     });
 
 
-    // Interface
-    it('An active user in the users list has an option to deactivate it');
-
-
     // Actions
-    it('The admin user can deactivate an active user');
+    it('The admin user can deactivate an active user', function(){
+
+        // Setup - add a new user
+        UserManagementActions.adminAddsNewUser();
+
+        const defaultUser = {
+            userName:       DefaultUserDetails.NEW_USER_NAME,
+            password:       DefaultUserDetails.NEW_USER_PASSWORD,
+            displayName:    DefaultUserDetails.NEW_USER_DISPLAY_NAME,
+            isDesigner:     false,
+            isDeveloper:    false,
+            isManager:      false,
+            isAdmin:        false,
+            isActive:       true
+        };
+
+        expect(UserManagementVerifications.userDetailsAre(defaultUser));
+
+        // Execute - deactivate
+        UserManagementActions.adminDeactivatesUser(DefaultUserDetails.NEW_USER_NAME);
+
+        // Verify
+        expect(UserManagementVerifications.userIsInactive(DefaultUserDetails.NEW_USER_NAME));
+    });
 
 
     // Conditions
-    it('An inactive user cannot be deactivated');
+    it('Only the admin user can deactivate a user', function(){
 
-    it('Only the admin user can deactivate a user');
+        // Setup - add a new user
+        UserManagementActions.adminAddsNewUser();
 
+        // Expect all to fail
+        const expectation = {success: false, message: UserManagementValidationErrors.USER_MANAGEMENT_INVALID_USER_DEACTIVATE};
 
-    // Consequences
-    it('When a user is deactivated it is no longer possible to log in with that username');
+        // Designer
+        UserManagementActions.designerDeactivatesUser(DefaultUserDetails.NEW_USER_NAME, expectation);
+        expect(UserManagementVerifications.userIsActive(DefaultUserDetails.NEW_USER_NAME));
+
+        // Developer
+        UserManagementActions.developerDeactivatesUser(DefaultUserDetails.NEW_USER_NAME, expectation);
+        expect(UserManagementVerifications.userIsActive(DefaultUserDetails.NEW_USER_NAME));
+
+        // Manager
+        UserManagementActions.managerDeactivatesUser(DefaultUserDetails.NEW_USER_NAME, expectation);
+        expect(UserManagementVerifications.userIsActive(DefaultUserDetails.NEW_USER_NAME));
+    });
+
 
 });
