7be4ff9b305784c9b58b27bf1f14e9be4431f954
aston <aston.cockayne@blueyonder.co.uk>
2017-03-09 16:56:07 +0000

    Unit testing - Design Update Scope

--- imports/ui/components/edit/DesignComponentHeader_UpdateComponent.tests.js
+++ imports/ui/components/edit/DesignComponentHeader_UpdateComponent.tests.js
@@ -47,6 +47,7 @@ describe('JSX: DesignComponentHeader', () => {
 
     // Scope Components ------------------------------------------------------------------------------------------------
 
+    // Open Close
     describe('Each Design Component in Design Update scope has a toggle to open or close it', () => {
 
         it('an application has open-close option in scope pane', () => {
@@ -118,6 +119,128 @@ describe('JSX: DesignComponentHeader', () => {
             const item = designComponentHeaderTest(currentItem, mode, view, displayContext);
 
             chai.assert.equal(item.find('#openCloseIcon').props().className, 'invisible', 'Open-close option was visible!');
+
+        });
+    });
+
+    // Put in scope
+    describe('A Feature in the Design Update Scope pane has a toggle that allows it to be put in scope', () => {
+
+        it('non scoped feature has an unchecked toggle', () => {
+
+            const currentItem = {componentType: ComponentType.FEATURE, isInScope: false, isScopable: true};
+            const mode = ViewMode.MODE_EDIT;
+            const view = ViewType.DESIGN_UPDATE_EDIT;
+            const displayContext = DisplayContext.UPDATE_SCOPE;
+
+            let item = designComponentHeaderTest(currentItem, mode, view, displayContext);
+
+            // Ensure box cleared
+            item.setState({inScope: false});
+
+            chai.assert.equal(item.find('#scopeCheckBox').length, 1, 'Scoping box not visible');
+            chai.assert.equal(item.find('#scopeCheckBox').props().className, 'out-scope', 'Scoping box is checked');
+
+        });
+    });
+
+    describe('A Feature Aspect in the Design Update Scope pane has a toggle that allows it to be put in scope', () => {
+
+        it('non scoped feature aspect has an unchecked toggle', () => {
+
+            const currentItem = {componentType: ComponentType.FEATURE_ASPECT, isInScope: false, isScopable: true};
+            const mode = ViewMode.MODE_EDIT;
+            const view = ViewType.DESIGN_UPDATE_EDIT;
+            const displayContext = DisplayContext.UPDATE_SCOPE;
+
+            let item = designComponentHeaderTest(currentItem, mode, view, displayContext);
+
+            // Ensure box cleared
+            item.setState({inScope: false});
+
+            chai.assert.equal(item.find('#scopeCheckBox').length, 1, 'Scoping box not visible');
+            chai.assert.equal(item.find('#scopeCheckBox').props().className, 'out-scope', 'Scoping box is checked');
+
+        });
+    });
+
+    describe('A Scenario in the Design Update Scope pane has a toggle that allows it to be put in scope', () => {
+
+        it('non scoped scenario has an unchecked toggle', () => {
+
+            const currentItem = {componentType: ComponentType.SCENARIO, isInScope: false, isScopable: true};
+            const mode = ViewMode.MODE_EDIT;
+            const view = ViewType.DESIGN_UPDATE_EDIT;
+            const displayContext = DisplayContext.UPDATE_SCOPE;
+
+            let item = designComponentHeaderTest(currentItem, mode, view, displayContext);
+
+            // Ensure box cleared
+            item.setState({inScope: false});
+
+            chai.assert.equal(item.find('#scopeCheckBox').length, 1, 'Scoping box not visible');
+            chai.assert.equal(item.find('#scopeCheckBox').props().className, 'out-scope', 'Scoping box is checked');
+
+        });
+    });
+
+    // Put out of scope
+    describe('A Feature in the Design Update Scope pane has a toggle that allows it to be put out of scope', () => {
+
+        it('non scoped feature has an unchecked toggle', () => {
+
+            const currentItem = {componentType: ComponentType.FEATURE, isInScope: true, isScopable: true};
+            const mode = ViewMode.MODE_EDIT;
+            const view = ViewType.DESIGN_UPDATE_EDIT;
+            const displayContext = DisplayContext.UPDATE_SCOPE;
+
+            let item = designComponentHeaderTest(currentItem, mode, view, displayContext);
+
+            // Simulate checking the box
+            item.setState({inScope: true});
+
+            chai.assert.equal(item.find('#scopeCheckBox').length, 1, 'Scoping box not visible');
+            chai.assert.equal(item.find('#scopeCheckBox').props().className, 'in-scope', 'Scoping box is not checked');
+
+        });
+    });
+
+    describe('A Feature Aspect in the Design Update Scope pane has a toggle that allows it to be put out of scope', () => {
+
+        it('non scoped feature aspect has an unchecked toggle', () => {
+
+            const currentItem = {componentType: ComponentType.FEATURE_ASPECT, isInScope: true, isScopable: true};
+            const mode = ViewMode.MODE_EDIT;
+            const view = ViewType.DESIGN_UPDATE_EDIT;
+            const displayContext = DisplayContext.UPDATE_SCOPE;
+
+            let item = designComponentHeaderTest(currentItem, mode, view, displayContext);
+
+            // Simulate checking the box
+            item.setState({inScope: true});
+
+            chai.assert.equal(item.find('#scopeCheckBox').length, 1, 'Scoping box not visible');
+            chai.assert.equal(item.find('#scopeCheckBox').props().className, 'in-scope', 'Scoping box is not checked');
+
+        });
+    });
+
+    describe('A Scenario in the Design Update Scope pane has a toggle that allows it to be put out of scope', () => {
+
+        it('non scoped scenario has an unchecked toggle', () => {
+
+            const currentItem = {componentType: ComponentType.SCENARIO, isInScope: true, isScopable: true};
+            const mode = ViewMode.MODE_EDIT;
+            const view = ViewType.DESIGN_UPDATE_EDIT;
+            const displayContext = DisplayContext.UPDATE_SCOPE;
+
+            let item = designComponentHeaderTest(currentItem, mode, view, displayContext);
+
+            // Simulate checking the box
+            item.setState({inScope: true});
+
+            chai.assert.equal(item.find('#scopeCheckBox').length, 1, 'Scoping box not visible');
+            chai.assert.equal(item.find('#scopeCheckBox').props().className, 'in-scope', 'Scoping box is not checked');
 
         });
     });
