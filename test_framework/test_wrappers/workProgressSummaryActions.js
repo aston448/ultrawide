7b79082f183fbae93d7f37b6d5c5d202c53b819b
aston <aston.cockayne@blueyonder.co.uk>
2017-04-27 11:40:37 +0100

    UC 402 Integration Tests

--- test_framework/test_wrappers/workProgressSummaryActions.js
+++ test_framework/test_wrappers/workProgressSummaryActions.js
@@ -7,67 +7,67 @@ class WorkProgressSummaryActions {
     // Goto DVs
 
     designerGoesToInitialDesignVersion(designVersionName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_DV, designVersionName, RoleType.DESIGNER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_DV, designVersionName, RoleType.DESIGNER, 'gloria', expectation);
     }
 
     developerGoesToInitialDesignVersion(designVersionName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_DV, designVersionName, RoleType.DEVELOPER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_DV, designVersionName, RoleType.DEVELOPER, 'hugh', expectation);
     }
 
     managerGoesToInitialDesignVersion(designVersionName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_DV, designVersionName, RoleType.MANAGER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_DV, designVersionName, RoleType.MANAGER, 'miles', expectation);
     }
 
     designerGoesToUpdatableDesignVersion(designVersionName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_DV, designVersionName, RoleType.DESIGNER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_DV, designVersionName, RoleType.DESIGNER, 'gloria', expectation);
     }
 
     developerGoesToUpdatableDesignVersion(designVersionName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_DV, designVersionName, RoleType.DEVELOPER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_DV, designVersionName, RoleType.DEVELOPER, 'hugh', expectation);
     }
 
     managerGoesToUpdatableDesignVersion(designVersionName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_DV, designVersionName, RoleType.MANAGER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_DV, designVersionName, RoleType.MANAGER, 'miles', expectation);
     }
 
     // Goto DUs
 
     designerGoesToDesignUpdate(designUpdateName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE, designUpdateName, RoleType.DESIGNER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE, designUpdateName, RoleType.DESIGNER, 'gloria', expectation);
     }
 
     developerGoesToDesignUpdate(designUpdateName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE, designUpdateName, RoleType.DEVELOPER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE, designUpdateName, RoleType.DEVELOPER, 'hugh', expectation);
     }
 
     managerGoesToDesignUpdate(designUpdateName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE, designUpdateName, RoleType.MANAGER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE, designUpdateName, RoleType.MANAGER, 'miles', expectation);
     }
 
     // Goto WPs
 
     designerGoesToBaseWorkPackage(workPackageName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_WP, workPackageName, RoleType.DESIGNER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_WP, workPackageName, RoleType.DESIGNER, 'gloria', expectation);
     }
 
     developerGoesToBaseWorkPackage(workPackageName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_WP, workPackageName, RoleType.DEVELOPER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_WP, workPackageName, RoleType.DEVELOPER, 'hugh', expectation);
     }
 
     managerGoesToBaseWorkPackage(workPackageName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_WP, workPackageName, RoleType.MANAGER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_BASE_WP, workPackageName, RoleType.MANAGER, 'miles', expectation);
     }
 
     designerGoesToUpdateWorkPackage(workPackageName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_WP, workPackageName, RoleType.DESIGNER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_WP, workPackageName, RoleType.DESIGNER, 'gloria', expectation);
     }
 
     developerGoesToUpdateWorkPackage(workPackageName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_WP, workPackageName, RoleType.DEVELOPER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_WP, workPackageName, RoleType.DEVELOPER, 'hugh', expectation);
     }
 
     managerGoesToUpdateWorkPackage(workPackageName, expectation) {
-        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_WP, workPackageName, RoleType.MANAGER, expectation);
+        server.call('testWorkProgress.gotoWorkItem', WorkSummaryType.WORK_SUMMARY_UPDATE_WP, workPackageName, RoleType.MANAGER, 'miles', expectation);
     }
 }
 export default new WorkProgressSummaryActions()
