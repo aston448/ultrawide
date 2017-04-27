f7c8e2cf9cdaf25c45e1f0e789e995fc5f2d7313
aston <aston.cockayne@blueyonder.co.uk>
2017-04-27 11:37:31 +0100

    UC 402 Integration Tests

--- test_framework/apiTest/apiClientTestWorkProgressSummary.js
+++ test_framework/apiTest/apiClientTestWorkProgressSummary.js
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import { DesignVersions }           from '../../imports/collections/design/design_versions.js';
 import { DesignUpdates }            from '../../imports/collections/design_update/design_updates.js';
 import { WorkPackages }             from '../../imports/collections/work/work_packages.js';
+import { UserWorkProgressSummary }  from '../../imports/collections/summary/user_work_progress_summary.js';
 
 import ClientDesignVersionServices    from '../../imports/apiClient/apiClientDesignVersion.js';
 import TestDataHelpers              from '../test_modules/test_data_helpers.js'
@@ -13,11 +14,14 @@ import {RoleType, WorkSummaryType, ViewType, ViewMode, DisplayContext, Component
 
 Meteor.methods({
 
-    'testWorkProgress.gotoWorkItem'(itemType, itemName, asRole, expectation){
+    'testWorkProgress.gotoWorkItem'(itemType, itemName, asRole, userName, expectation){
 
         expectation = TestDataHelpers.getExpectation(expectation);
 
+        const userContext = TestDataHelpers.getUserContext(userName);
+
         let item = null;
+        let workSummaryItem = null;
 
         switch(itemType){
             case WorkSummaryType.WORK_SUMMARY_BASE_DV:
@@ -25,27 +29,60 @@ Meteor.methods({
 
                 item = DesignVersions.findOne({designVersionName: itemName});
 
+                if(item){
+                    workSummaryItem = UserWorkProgressSummary.findOne({
+                        userId:                     userContext.userId,
+                        designVersionId:            item._id,
+                        designUpdateId:             'NONE',
+                        workPackageId:              'NONE'
+                    });
+                } else {
+                    throw new Meteor.Error("NOT_FOUND", "Work item called " + itemName + " not found");
+                }
+
                 break;
             case WorkSummaryType.WORK_SUMMARY_UPDATE:
 
                 item = DesignUpdates.findOne({updateName: itemName});
+
+                if(item){
+                    workSummaryItem = UserWorkProgressSummary.findOne({
+                        userId:                     userContext.userId,
+                        designVersionId:            item.designVersionId,
+                        designUpdateId:             item._id,
+                        workPackageId:              'NONE'
+                    });
+                } else {
+                    throw new Meteor.Error("NOT_FOUND", "Work item called " + itemName + " not found");
+                }
 
                 break;
             case WorkSummaryType.WORK_SUMMARY_BASE_WP:
             case WorkSummaryType.WORK_SUMMARY_UPDATE_WP:
 
                 item = WorkPackages.find({workPackageName: itemName});
+
+                if(item){
+                    workSummaryItem = UserWorkProgressSummary.findOne({
+                        userId:                     userContext.userId,
+                        designVersionId:            item.designVersionId,
+                        designUpdateId:             item.designUpdateId,
+                        workPackageId:              item._id
+                    });
+                } else {
+                    throw new Meteor.Error("NOT_FOUND", "Work item called " + itemName + " not found");
+                }
                 break;
         }
 
-        if(item){
+        if(workSummaryItem){
 
-            const outcome = ClientDesignVersionServices.gotoWorkProgressSummaryItemAsRole(item, asRole);
+            const outcome = ClientDesignVersionServices.gotoWorkProgressSummaryItemAsRole(workSummaryItem, asRole);
 
             TestDataHelpers.processClientCallOutcome(outcome, expectation, 'Goto Item as Role');
 
         } else {
-            throw new Meteor.Error("NOT_FOUND", "Work item called " + itemName + " not found");
+            throw new Meteor.Error("NOT_FOUND", "Work summary item not found for " + itemName);
         }
 
     },
