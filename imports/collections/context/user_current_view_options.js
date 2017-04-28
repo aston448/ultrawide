7ffc4ec4cb95f6205e4b02716acf914bac536020
aston <aston.cockayne@blueyonder.co.uk>
2017-04-28 12:56:26 +0100

    UC508 Integration Tests

--- imports/collections/context/user_current_view_options.js
+++ imports/collections/context/user_current_view_options.js
@@ -8,12 +8,12 @@ export const UserCurrentViewOptions = new Mongo.Collection('userCurrentViewOptio
 let Schema = new SimpleSchema({
     userId:                     {type: String},                             // Meteor user id
     // All screens
-    designDetailsVisible:       {type: Boolean, defaultValue: true},        // The details pane
-    designDomainDictVisible:    {type: Boolean, defaultValue: true},        // Domain Dictionary pane
+    designDetailsVisible:       {type: Boolean, defaultValue: false},        // The details pane
+    designDomainDictVisible:    {type: Boolean, defaultValue: false},        // Domain Dictionary pane
     testSummaryVisible:         {type: Boolean, defaultValue: false},       // Test Summary add-on
     // Design Updates specific
     updateProgressVisible:      {type: Boolean, defaultValue: false},       // The merged version pane
-    updateSummaryVisible:       {type: Boolean, defaultValue: true},        // Update Summary pane
+    updateSummaryVisible:       {type: Boolean, defaultValue: false},        // Update Summary pane
     // Test Specific
     devAccTestsVisible:         {type: Boolean, defaultValue: false},        // Acceptance Tests view
     devIntTestsVisible:         {type: Boolean, defaultValue: false},        // Integration Tests view
