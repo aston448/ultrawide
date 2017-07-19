e16529571ae5b3e63cc2e68243a08f5b3b500fc3
aston <aston.cockayne@blueyonder.co.uk>
2017-07-19 20:56:30 +0100

    remove idea

---
+++ tests/integration/UC 402 - View Work Package Progress Summary_spec.js
@@ -0,0 +1,73 @@
+describe('UC 402 - View Work Package Progress Summary', function(){
+
+    before(function(){
+
+    });
+
+    after(function(){
+
+    });
+
+    beforeEach(function(){
+
+    });
+
+    afterEach(function(){
+
+    });
+
+
+    // Interface
+    it('The number of scenarios in the current Design Version is displayed');
+
+    it('The number of Scenarios covered by Work Packages in a Base Design Version is displayed');
+
+    it('The number of Scenarios with passing tests in the current Design Version is displayed');
+
+    it('The number of Scenarios with failing tests in the current Design Version is displayed');
+
+    it('The number of Scenarios with no tests in the current Design Version is displayed');
+
+    it('Each Design Update to be included in an Updatable Design Version is listed');
+
+    it('The number of Scenarios in a Design Update is displayed');
+
+    it('The number of Scenarios covered by Work Packages in a design Update is displayed');
+
+    it('The number of Scenarios with passing tests in a Design Update is displayed');
+
+    it('The number of Scenarios with failing tests in a Design Update is displayed');
+
+    it('The number of Scenarios with no tests in a Design Update is displayed');
+
+    it('Each Work Package in a Base Design Version is displayed');
+
+    it('Each Work Package in a Design Update is displayed');
+
+    it('The number of Scenarios in a Work Package is displayed');
+
+    it('The number of Scenarios with passing tests in a Work Package is displayed');
+
+    it('The number of Scenarios with failing tests in a Work Package is displayed');
+
+    it('The number of Scenarios with no tests in a Work Package is displayed');
+
+
+    // Actions
+    it('Work progress summary data is refreshed when the summary is viewed');
+
+    it('The user can navigate to a Design Update displayed in the work progress summary');
+
+    it('The user can navigate to a Work Package displayed in the work progress summary');
+
+    it('The user can navigate to an item in the work progress summary as any role available to the user');
+
+
+    // Conditions
+    it('Design Updates that are not yet Published are not displayed');
+
+    it('Design Updates that are not included in the Design Version are not displayed');
+
+    it('Work Packages that are not yet Available are not displayed');
+
+});
