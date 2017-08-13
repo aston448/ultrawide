ce4318ab2c6f3eb4d283c1ae693260f8204fb4a5
aston <aston.cockayne@blueyonder.co.uk>
2017-08-13 19:34:42 +0100

    Integrate chimp testing

---
+++ scripts/upload-test-results.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FILE_NAME=$1
+
+curl -i -H "name: $FILE_NAME" -H "location: Ultrawide Dev Test Outputs" -H "key: lySGChJwdI6OKO3e" -H "Content-Type:application/json" -X POST --data-binary @.test_results/$FILE_NAME http://178.62.78.222/api/v1/upload-file
