e0fa002d77fe826d52a075874cf85cd13692fbef
aston <aston.cockayne@blueyonder.co.uk>
2017-12-18 22:25:29 +0000

    Add new acceptance test framework

--- scripts/upload-test-results.sh
+++ scripts/upload-test-results.sh
@@ -2,4 +2,4 @@
 
 FILE_NAME=$1
 
-curl -i -H "name: $FILE_NAME" -H "location: Ultrawide Dev Test Outputs" -H "key: lySGChJwdI6OKO3e" -H "Content-Type:application/json" -X POST --data-binary @/home/circleci/project/.test_results/$FILE_NAME http://178.62.78.222/api/v1/upload-file
+curl -i -H "name: $FILE_NAME" -H "location: Ultrawide CI Test Outputs" -H "key: lySGChJwdI6OKO3e" -H "Content-Type:application/json" -X POST --data-binary @/home/circleci/project/.test_results/$FILE_NAME http://178.62.78.222/api/v1/upload-file
