381dbbb767ce21d0172229901f860c8e1c9011aa
aston <aston.cockayne@blueyonder.co.uk>
2018-06-24 16:59:13 +0100

    Fixes to full baseline info

---
+++ tests/integration/UC 823 - Archive Design_spec.js
@@ -0,0 +1,34 @@
+describe('UC 823 - Archive Design', function(){
+
+    before(function(){
+
+    });
+
+    after(function(){
+
+    });
+
+    beforeEach(function(){
+
+    });
+
+
+
+    // Interface
+    it('A live Design in the Designs administration list has an option to archive it');
+
+
+    // Actions
+    it('The admin user may archive a live Design');
+
+
+    // Conditions
+    it('A removable Design may not be archived');
+
+    it('Designer, Developer and Manager users cannot archive a Design');
+
+
+    // Consequences
+    it('When a Design is archived it is backed up and all Design data is removed from the Ultrawide database');
+
+});
